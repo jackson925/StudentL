bef8b84f5f5d8c3f86598fb99cac15a57bcb38b8
Jordan L. Jackson <jordanjackson925@gmail.com>
2017-06-12 10:18:57 -0400

    Initial Commit

---
+++ StudentLeaf/Controllers/API/StudentController.cs
@@ -0,0 +1,29 @@
+﻿using StudentLeaf.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace StudentLeaf.Controllers.API
+{
+    public class StudentController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public StudentController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        //GET /api/students
+        public IEnumerable<Student> GetStudents()
+        {
+            return _context.Student.ToList();
+        }
+        public IEnumerable<ActiveLesson> GetLessons()
+        {
+            return _context.ActiveLessons.ToList();
+        }
+    }
+}

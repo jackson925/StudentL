83092249eee469e5ce625b8879be97cd367eff38
JordanJackson <jordanjackson925@gmail.com>
2017-07-09 15:07:37 -0700

    InstructorController, Dynamic History added

---
+++ StudentLeaf/Controllers/InstructorController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StudentLeaf.Models;
+
+namespace StudentLeaf.Controllers
+{
+    public class InstructorController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public InstructorController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        // GET: Instructor
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult List()
+        {
+            var instructors = _context.Instructor.ToList();
+            return Json(instructors, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
